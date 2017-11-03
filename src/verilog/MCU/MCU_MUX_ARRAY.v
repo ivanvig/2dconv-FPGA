8111bc3dc11fbdcfba0ee7a6c5c5b77a81a1836b
ivanvig <ivanmvig@gmail.com>
2017-11-03 13:26:45 -0300

    reescrito todo el MUX_ARRAY

--- src/verilog/MCU/MCU_MUX_ARRAY.v
+++ src/verilog/MCU/MCU_MUX_ARRAY.v
@@ -1,67 +1,52 @@
- module MUX_ARRAY #(
-        parameter N = 2,
-        parameter BITS_IMAGEN = 8
-    )(
+module MUX_ARRAY
+    #(
+      parameter N = 2,
+      parameter BITS_IMAGEN = 11,
+      parameter BITS_DATA = BITS_IMAGEN,
+      parameter STATES = 3
+      )(
         input [N*BITS_IMAGEN-1:0]      i_DataConv,
         input [(N+2)*BITS_IMAGEN-1:0]  i_MemData,
-        input [23:0]                   i_Data,
+        input [BITS_DATA-1:0]          i_Data,
 
-        input                          i_inputCtrl,
-		    input                          i_memCtrl,
-		    input                          i_convCtrl,
+        input [$clog2(STATES)-1:0]     i_state;
+        input [$clog2(N/2 + 1):0]      i_substate; //PARA N PAR
+        input [$clog2(N+2)-1:0]        i_memSelect;
+        //input                          i_inputCtrl,
+		    //input [$clog2(N/2 + 1)-1:0]    i_memCtrl, //Para N par
+		    //input                          i_convCtrl,
         //CONFIGURAR BIEN LAS SELECTORAS
         output [3*N*BITS_IMAGEN-1:0]   o_DataConv,
         output [(N+2)*BITS_IMAGEN-1:0] o_MemData,
-        output [3*BITS_IMAGEN-1:0]     o_Data     //Revisar esto
+        output [BITS_DATA-1:0]         o_Data
     );
-
-    wire	[N*8-1:0]	tomemory;
-
-    assign o_DataConv[23:0] = i_convCtrl ? {i_MemData[7:0],i_MemData[31:16]} : i_MemData[23:0];
-    assign o_DataConv[47:24] = i_convCtrl ? {i_MemData[15:0],i_MemData[31:24]} : i_MemData[31:8];
-
-    assign tomemory	= i_inputCtrl ? i_DataConv : i_Data[15:0];
-
-	  assign o_MemData[15:0] 	= i_memCtrl ? 16'b0 : tomemory;
-	  assign o_MemData[31:16] = i_memCtrl ? tomemory : 16'b0;
-
-
-    assign o_Data[15:0] = i_memCtrl ? i_MemData[31:16] : i_MemData[15:0];
-    assign o_Data[23:16] = 8'b0;
-/*
-
-ASDASD
-    always@(*) begin
-
-    	//SALIDAS A LA MEMORIA
-        case (i_inputCtrl)
-        	1'b0 : assign tomemory	= i_Data[15:0];
-        	1'b1 : assign tomemory = i_DataConv;
-        endcase
-
-        case (i_memCtrl)
-        	1'b0 : begin
-        		assign o_MemData[15:0] 	= tomemory;
-        		assign o_MemData[31:16] = 16'h0;
-        	end
-        	1'b1 : begin
-        		assign o_MemData[15:0] 	= 16'h0;
-        		assign o_MemData[31:16] = tomemory;
-        	end
-        endcase
-
-        //SALIDAS AL CONV
-        case(i_convCtrl)
-        	1'b0 : begin
-        		assign o_DataConv[23:0]	 = i_MemData[23:0];
-        		assign o_DataConv[47:24] = i_MemData[31:8];
-        	end
-        	1'b1 : begin
-        		assign o_DataConv[23:0] = {i_MemData[7:0],i_MemData[31:16]};
-        		assign o_DataConv[47:24] = {i_MemData[15:0],i_MemData[31:24]};
-        		end
-        endcase
     
-    end*/
+    wire [N*BITS_IMAGEN-1:0]           tomemory;
 
-endmodule
+    always @ ( /*AUTOSENSE*/ ) begin
+        case (i_state)
+            2'b00:
+                case (i_memSelect) 
+                    2'b00: o_MemData = i_Data;
+                    2'b01: o_MemData = {i_Data, {1*BITS_IMAGEN{1'b0}}};
+                    2'b10: o_MemData = {i_Data, {2*BITS_IMAGEN{1'b0}}}
+                    2'b11: o_MemData = {i_Data, {3*BITS_IMAGEN{1'b0}}}
+                endcase
+            2'b01: 
+                case (i_substate) 
+                    1'b0: begin
+                        o_DataConv[3*BITS_IMAGEN-1:0] = i_MemData[3*BITS_IMAGEN-1:0];
+                        o_DataConv[6*BITS_IMAGEN-1:3*BITS_IMAGEN] = i_MemData[4*BITS_IMAGEN-1:BITS_IMAGEN];
+                        o_MemData = i_DataConv;
+                    end
+                    1'b1: begin
+                        o_DataConv[3*BITS_IMAGEN-1:0] = {i_MemData[BITS_IMAGEN-1:0],i_MemData[4*BITS_IMAGEN-1:2*BITS_IMAGEN]};
+                        o_DataConv[6*BITS_IMAGEN-1:3*BITS_IMAGEN] = {i_MemData[2*BITS_IMAGEN-1:0] ,i_MemData[4*BITS_IMAGEN-1:3*BITS_IMAGEN]};
+                        o_MemData = {i_DataConv, {2*BITS_IMAGEN{1'b0}}};
+                    end
+                endcase
+
+            2'b10: o_Data = i_MemData[(1+i_memSelect)*BITS_IMAGEN-1:i_memSelect*BITS_IMAGEN];
+            
+        endcase
+    end
