bc419ac5e6ee40a1530824102037e5d0d7842177
Ivan Vig <ivanmvig@gmail.com>
2017-11-13 12:46:15 -0300

    MUX ARRAY y TB terminado

--- src/verilog/MCU/MCU_MUX_ARRAY.v
+++ src/verilog/MCU/MCU_MUX_ARRAY.v
@@ -9,30 +9,35 @@ module MUX_ARRAY
         input [(N+2)*BITS_IMAGEN-1:0]  i_MemData,
         input [BITS_DATA-1:0]          i_Data,
 
-        input [$clog2(STATES)-1:0]     i_state;
-        input [$clog2(N/2 + 1):0]      i_substate; //PARA N PAR
-        input [$clog2(N+2)-1:0]        i_memSelect;
+        input [$clog2(STATES)-1:0]     i_state,
+        input [$clog2(N/2 + 1):0]      i_substate, //PARA N PAR
+        input [$clog2(N+2)-1:0]        i_memSelect,
         //input                          i_inputCtrl,
 		    //input [$clog2(N/2 + 1)-1:0]    i_memCtrl, //Para N par
 		    //input                          i_convCtrl,
         //CONFIGURAR BIEN LAS SELECTORAS
-        output [3*N*BITS_IMAGEN-1:0]   o_DataConv,
-        output [(N+2)*BITS_IMAGEN-1:0] o_MemData,
-        output [BITS_DATA-1:0]         o_Data
+        output reg [3*N*BITS_IMAGEN-1:0]   o_DataConv,
+        output reg [(N+2)*BITS_IMAGEN-1:0] o_MemData,
+        output reg [BITS_DATA-1:0]         o_Data
     );
     
     wire [N*BITS_IMAGEN-1:0]           tomemory;
 
-    always @ ( /*AUTOSENSE*/ ) begin
+    always @ (*) begin
         case (i_state)
-            2'b00:
+            2'b00: begin//LOAD
+                o_Data = 0;
+                o_DataConv = 0;
                 case (i_memSelect) 
                     2'b00: o_MemData = i_Data;
                     2'b01: o_MemData = {i_Data, {1*BITS_IMAGEN{1'b0}}};
-                    2'b10: o_MemData = {i_Data, {2*BITS_IMAGEN{1'b0}}}
-                    2'b11: o_MemData = {i_Data, {3*BITS_IMAGEN{1'b0}}}
+                    2'b10: o_MemData = {i_Data, {2*BITS_IMAGEN{1'b0}}};
+                    2'b11: o_MemData = {i_Data, {3*BITS_IMAGEN{1'b0}}};
                 endcase
-            2'b01: 
+            end
+
+            2'b01: begin //PROCESAMIENTO
+                o_Data = 0;
                 case (i_substate) 
                     1'b0: begin
                         o_DataConv[3*BITS_IMAGEN-1:0] = i_MemData[3*BITS_IMAGEN-1:0];
@@ -45,8 +50,26 @@ module MUX_ARRAY
                         o_MemData = {i_DataConv, {2*BITS_IMAGEN{1'b0}}};
                     end
                 endcase
+            end
 
-            2'b10: o_Data = i_MemData[(1+i_memSelect)*BITS_IMAGEN-1:i_memSelect*BITS_IMAGEN];
-            
+            2'b10: begin//OUT
+                o_MemData = 0;
+                o_DataConv = 0;
+                
+                case (i_memSelect)
+                    2'b00: o_Data = i_MemData[BITS_IMAGEN-1:0];
+                    2'b01: o_Data = i_MemData[2*BITS_IMAGEN-1:BITS_IMAGEN];
+                    2'b10: o_Data = i_MemData[3*BITS_IMAGEN-1:2*BITS_IMAGEN];
+                    2'b11: o_Data = i_MemData[4*BITS_IMAGEN-1:3*BITS_IMAGEN];
+                endcase
+                //o_Data = i_MemData[(1+i_memSelect)*BITS_IMAGEN-1:i_memSelect*BITS_IMAGEN];
+            end
+
+            default: begin
+                o_MemData = 0;
+                o_DataConv = 0;
+                o_Data = 0;
+            end
         endcase
     end
+endmodule
--- src/verilog/MCU/MCU_MUX_ARRAY_TB.v
+++ src/verilog/MCU/MCU_MUX_ARRAY_TB.v
@@ -1,53 +1,68 @@
 module tb_MUX_ARRAY();
-
-    parameter N_CONV = 2;
+    
+    parameter N = 2;
     parameter BITS_IMAGEN = 8;
-    
-    reg [N_CONV*BITS_IMAGEN-1:0] i_DataConv;
-    reg [(N_CONV+2)*BITS_IMAGEN-1:0]  i_MemData;
-    reg [23:0]                   Data_in;
-    reg                          inputCtrl, memCtrl, convCtrl;
-    
-    wire [3*N_CONV*BITS_IMAGEN-1:0] o_DataConv;
-    wire [(N_CONV+2)*BITS_IMAGEN-1:0] o_MemData;
-    wire [3*BITS_IMAGEN-1:0]        Data_out;
+    parameter STATES = 3;
+    parameter BITS_DATA = BITS_IMAGEN;
 
+    
+    reg [N*BITS_IMAGEN-1:0]          i_DataConv;
+    reg [(N+2)*BITS_IMAGEN-1:0]      i_MemData;
+    reg [BITS_DATA-1:0]              i_Data;
+
+    reg [$clog2(STATES)-1:0]         i_state;
+    reg [$clog2(N/2 + 1):0]          i_substate; //PARA N PAR
+    reg [$clog2(N+2)-1:0]            i_memSelect;
+
+    wire [3*N*BITS_IMAGEN-1:0]       o_DataConv;
+    wire [(N+2)*BITS_IMAGEN-1:0]     o_MemData;
+    wire [BITS_DATA-1:0]               o_Data;
+    
 
     initial begin
 
-        Data_in    = {12'b1, 12'b0};
-        i_MemData    = {8'b0, 8'b1, 8'b0, 8'b1};
-        i_DataConv = 16'b1;
-
-        inputCtrl = 0;
-        memCtrl   = 0;
-        convCtrl  = 0;
-
-        #100 memCtrl = 1;
-        #200 memCtrl = 0;
-        #200 inputCtrl = 1;
-        #300 convCtrl = 1;
-        #300 memCtrl = 1;
-        #500 $finish;
+        i_Data     = {BITS_DATA{1'b1,1'b0}};
+        i_MemData  = {8'b0, 8'b1, 8'b0, 8'b1};
+        i_DataConv = {16{1'b1}};
+        
+        i_state     = 0;
+        i_substate  = 0;
+        i_memSelect = 0;
+        
+        #100 i_memSelect = 1;
+        #200 i_memSelect = 2;
+        #300 i_memSelect = 3;
+        
+        #350 i_state = 1;
+        #500 i_substate = 1;
+        
+        #600 i_state  = 2;
+             i_memSelect = 0;
+        #650 i_memSelect = 1;
+        #700 i_memSelect = 2;
+        
+        #800 $finish;
 
     end
 
     MUX_ARRAY
         #(
-          .N(N_CONV),
-          .BITS_IMAGEN(BITS_IMAGEN)
+          .N(N),
+          .BITS_IMAGEN(BITS_IMAGEN),
+          .BITS_DATA(BITS_DATA),
+          .STATES(STATES)
           )
-
+    
     u_MUX_ARRAY
         (
          .i_DataConv(i_DataConv),
          .i_MemData(i_MemData),
-         .i_Data(Data_in),
-         .i_inputCtrl(inputCtrl),
-         .i_memCtrl(memCtrl),
-         .i_convCtrl(convCtrl),
+         .i_Data(i_Data),
+         .i_state(i_state),
+         .i_substate(i_substate),
+         .i_memSelect(i_memSelect),
          .o_DataConv(o_DataConv),
          .o_MemData(o_MemData),
-         .o_Data(Data_out)
+         .o_Data(o_Data)
          );
 endmodule
