30975ba31566e70f4578f96e2066cc66dc5edfe2
ivanvig <ivanmvig@gmail.com>
2017-12-05 10:57:43 -0300

    cambiado para leer txt binarios

--- src/verilog/Modules/Memory/memory.v
+++ src/verilog/Modules/Memory/memory.v
@@ -30,7 +30,7 @@ module memory( i_wrEnable, i_CLK, i_writeAdd, i_readAdd, i_data, o_data );
     generate
         if (INIT_FILE != "") begin: use_init_file
             initial
-                $readmemh(INIT_FILE, BRAM, 0, RAM_DEPTH-1);
+                $readmemb(INIT_FILE, BRAM, 0, RAM_DEPTH-1);
         end else begin: init_bram_to_zero
             integer ram_index;
             initial
