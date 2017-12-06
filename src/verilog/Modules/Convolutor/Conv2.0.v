b19b77123b5b4cecfa6247054910b6a8015a1877
ivanvig <ivanmvig@gmail.com>
2017-12-06 13:41:23 -0300

    cambiado kernel por defecto

--- src/verilog/Modules/Convolutor/Conv2.0.v
+++ src/verilog/Modules/Convolutor/Conv2.0.v
@@ -53,9 +53,9 @@ module Conv #(
             imagen[1]<=24'h0;
             imagen[2]<=24'h0;
             //reser valores de kernel
-            kernel[0]<=24'h0;
-            kernel[1]<=24'h100;
-            kernel[2]<=24'h0;
+            kernel[0]<=24'h002000;
+            kernel[1]<=24'h208020;
+            kernel[2]<=24'h002000;
             //regitro de la convolucion
             conv_reg<=0;
         end
