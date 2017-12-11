832f7cd3b5fa6ff3df7ea1a191ad5f297b29b995
Ivan Vig <ivanmvig@gmail.com>
2017-12-11 09:40:55 -0300

    agregadas mas funciones a memory generator

--- src/py/imgconv.py
+++ src/py/imgconv.py
@@ -40,3 +40,40 @@ def torange(matrix, max, min):
     matrix = np.true_divide((matrix-amin)*(max-min), (amax-amin) + min)
     return matrix
 
+def torange_c(matrix, max, min, amax, amin):
+    """
+    Implements a linear dynamic range expansion
+    https://en.wikipedia.org/wiki/Normalization_(image_processing)
+
+    :param matrix:matrix to be normalized
+    :param max:desired max value
+    :param min:desired min value
+    :return:normalized matrix
+    """
+    # amax = 0.0801  # np.amax(matrix)
+    # amin = -0.1  # np.amin(matrix)
+    matrix = np.true_divide((matrix-amin)*(max-min), (amax-amin)) + min
+    return matrix
+
+
+def ker_norm(matrix):
+    """
+
+    :param matrix:
+    :return:
+    """
+    if np.amin(matrix) * (-1) > np.amax(matrix):
+        amax = np.amin(matrix) * (-1)
+    else:
+        amax = np.amax(matrix)
+
+    matrix = np.true_divide(matrix, amax)
+    return matrix
+
+def pos(matrix):
+    shape = np.shape(matrix)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            # matrix[i][j] = (((matrix[i][j] & 0x7ffff) + 0x40000) >> 7) & 0xfff
+            matrix[i][j] = (((matrix[i][j] & 0xfffff) + 0x80000) >> 7) & 0x1fff
+    return matrix
