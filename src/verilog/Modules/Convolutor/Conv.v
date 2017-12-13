43c424384d91e6448144c2d3e3d1341170de083e
ser0090 <ser.0090@gmail.com>
2017-12-13 14:02:55 -0300

    nueva version de convolucionador con multiples arboles de suma

--- src/verilog/Modules/Convolutor/Conv.v
+++ src/verilog/Modules/Convolutor/Conv.v
@@ -1,21 +1,21 @@
 `timescale 1ns / 1ps
 
-`define BIT_LEN 8
-`define CONV_LEN 20
-`define CONV_LPOS 13
-`define M_LEN 3
+`define BIT_LEN     8
+`define CONV_LEN    20
+`define CONV_LPOS   13
+`define M_LEN       3
 
 module Conv #(
-    parameter BIT_LEN =`BIT_LEN,
-    parameter CONV_LEN =`CONV_LEN,
-    parameter CONV_LPOS = `CONV_LPOS,
-    parameter M_LEN = `M_LEN,
-    localparam BIT_ARRAY = BIT_LEN*3
+    parameter BIT_LEN       = `BIT_LEN,
+    parameter CONV_LEN      = `CONV_LEN,
+    parameter CONV_LPOS     = `CONV_LPOS,
+    parameter M_LEN         = `M_LEN,
+    localparam BIT_ARRAY    = BIT_LEN*M_LEN
     )(
-    output [CONV_LPOS-1:0] o_data,
-    input [BIT_LEN-1:0] i_dato0,
-    input [BIT_LEN-1:0] i_dato1,
-    input [BIT_LEN-1:0] i_dato2,
+    output [CONV_LPOS-1:0]  o_data,
+    input [BIT_LEN-1:0]     i_dato0,
+    input [BIT_LEN-1:0]     i_dato1,
+    input [BIT_LEN-1:0]     i_dato2,
     input i_selecK_I, //KI=0 modo kernle k=1 modo imagen
     input i_reset,
     input i_valid,
@@ -23,33 +23,31 @@ module Conv #(
     );
     // registros del kernel
     // [j][i] Primero columnas luego filas
-    reg signed [BIT_ARRAY-1:0] kernel [0:M_LEN-1];    
+    reg signed [BIT_ARRAY-1:0]  kernel [0:M_LEN-1];    
     // registros de la imagen
-    reg signed [BIT_ARRAY-1:0] imagen [0:M_LEN-1];
+    reg signed [BIT_ARRAY-1:0]  imagen [0:M_LEN-1];
     // resultado
     reg signed [CONV_LPOS-1:0]  conv_reg;
     // reg de la convolucion
-    reg signed [CONV_LEN-1:0] resultado;
     
-    integer ptr_row;
-    integer ptr_column;
+    reg signed [CONV_LEN-3:0]   parcial0,parcial1;
+
+    wire signed [CONV_LEN-1:0]   resultado;
+    
+    wire signed [(2*BIT_LEN)-1:0] array_prod [0:(M_LEN*M_LEN)-1];
+
+    integer ptr0 , ptr1;
+    integer shift;
 
     //Asigancion de Convolucion a la salida
     assign {o_data[CONV_LPOS-1],o_data[CONV_LPOS-2:0]}= {~conv_reg[CONV_LPOS-1], conv_reg[CONV_LPOS-2:0]};
-    integer shift;
     always @( posedge CLK100MHZ) begin
         if(i_reset) begin
-            //reset valores de imagen
+            //reset valores de imagen y kernel
             for(shift=0; shift < M_LEN; shift = shift +1) begin
                 imagen[shift]<={BIT_ARRAY{1'b0}};
                 kernel[shift]<={BIT_ARRAY{1'b0}};
             end 
-            //reser valores de kernel . modif
-            /*
-            kernel[0]<=24'h002000;
-            kernel[1]<=24'h208020;
-            kernel[2]<=24'h002000;
-            */
             //regitro de la convolucion
             conv_reg<={CONV_LPOS{1'b0}};
         end
@@ -59,19 +57,19 @@ module Conv #(
                     // imagen
                     for( shift = 0; shift < M_LEN-1; shift = shift +1)
                         imagen[shift]<=imagen[shift+1];
+                    
                     imagen[M_LEN-1]<={i_dato2,i_dato1,i_dato0};
                     //latcheo de la salida 
                     conv_reg<= resultado[CONV_LEN-1:CONV_LEN-CONV_LPOS];
-                    //conv_reg<= resultado;
                 end
                 1'b0: begin
                     //kernel
                     for( shift = 0; shift < M_LEN-1; shift = shift +1)
                         kernel[shift]<=kernel[shift+1];
+
                     kernel[M_LEN-1]<={i_dato2,i_dato1,i_dato0};
                     //salida
                     conv_reg<=conv_reg;
-
                 end
             endcase
         end
@@ -85,12 +83,37 @@ module Conv #(
             conv_reg<=conv_reg;
         end
     end
-    
     //Arbol de Suma
+    generate
+        genvar i;
+        for(i = 0 ; i < (M_LEN*M_LEN) ; i=i+1) 
+            assign array_prod[i] =  $signed(kernel[i/3][((i%3)+1)*BIT_LEN-1 -: BIT_LEN])*
+                                    $signed(imagen[i/3][((i%3)+1)*BIT_LEN-1 -: BIT_LEN]);
+        
+    endgenerate
+
+
+    always @(*) begin
+        parcial0=0;
+        for(ptr0 = 0 ; ptr0 < 4; ptr0 = ptr0 +1)begin
+            parcial0 = parcial0 + array_prod[ptr0];
+        end
+    end
+
+    always @(*) begin
+        parcial1=0;
+        for(ptr1 = 0 ; ptr1 < 4; ptr1 = ptr1 +1)begin
+            parcial1 = parcial1 + array_prod[4+ptr1]; 
+        end
+    end
+
+    assign  resultado = parcial0 + parcial1 + array_prod[8];
+    
+    /*
     always @(*) begin
         resultado = 20'h0;
         for(ptr_row = 0 ; ptr_row < M_LEN ; ptr_row=ptr_row+1) 
-        begin : SumaYMult
+        begin: SumaYMult
             for (ptr_column = 0 ; ptr_column < M_LEN ; ptr_column=ptr_column+1) 
             begin: SumaTop
                 resultado = resultado + $signed(kernel[ptr_row][(ptr_column+1)*BIT_LEN-1 -: BIT_LEN])*
@@ -98,4 +121,5 @@ module Conv #(
             end
         end
     end
+    */
 endmodule
